19f4d03ed33428a23c9598688440d422f6c2a23d
Pratusha Pandey <pratushapandey08@gmail.com>
2022-08-22 23:35:43 +0530

    Update RemoveDuplicates.java

--- Easy/RemoveDuplicates.java
+++ Easy/RemoveDuplicates.java
@@ -1,5 +1,3 @@
-import java.lang.*;
-import java.util.*;
 
 class Solution{
     public int removeDuplicates(int[]nums){
@@ -16,21 +14,4 @@ class Solution{
         nums[i++] = nums[nums.length-1];
         return i;
     }
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the elements of the array : ");
-        int n = sc.nextInt();
-        int [] nums = new int[n];
-        System.out.println("Enter the elements in the array : ");
-        for(int i = 0; i <n; i++){
-            nums[i] = sc.nextInt();
-        }
-        Solution sol = new Solution();
-        n = sol.removeDuplicates(nums);
-        System.out.println(n);
-        for (int i=0; i<n; i++) {
-        System.out.print(nums[i]+" ");  
-        }
-        sc.close();
-    }
-}
+   
