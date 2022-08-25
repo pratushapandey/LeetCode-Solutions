726d3fc268b8b941fe700507df99f57bfa2c3851
Pratusha Pandey <pratushapandey08@gmail.com>
2022-08-25 10:26:52 +0530

    Update SearchInsertPosition.java

--- Easy/SearchInsertPosition.java
+++ Easy/SearchInsertPosition.java
@@ -1,5 +1,3 @@
-import java.lang.*;
-import java.util.*;
 
 class Solution{
     public int searchInsert(int[]nums,int target){
@@ -9,22 +7,5 @@ class Solution{
             }
         }
         return nums.length;
-    }
-
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of elements in the array");
-        int n = sc.nextInt();
-        int []nums = new int[n];
-        System.out.println("Enter the elements :");
-        for(int i = 0;i <n;i++){
-            nums[i]= sc.nextInt();
-        }
-        System.out.println("Enter the target element :");
-        int target = n=sc.nextInt();
-        Solution s = new Solution();
-        int ans = s.searchInsert(nums,target);
-        System.out.println(ans);
-        sc.close();
     }
 }
