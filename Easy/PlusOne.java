188bab015b9f76e66be683df6dff7ac7bc926f84
Pratusha Pandey <pratushapandey08@gmail.com>
2022-08-25 10:49:13 +0530

    Update PlusOne.java

--- Easy/PlusOne.java
+++ Easy/PlusOne.java
@@ -1,5 +1,3 @@
-import java.lang.*;
-import java.util.*;
 
 class Solution{
     public int[] plusOne(int[]digits){
@@ -13,22 +11,5 @@ class Solution{
         int []newNumber = new int[digits.length+1];
         newNumber[0] = 1;
         return newNumber;
-    }
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of digits : ");
-        int n = sc.nextInt();
-        System.out.println("Enter the digits :");
-        int[]digits = new int[n];
-        for(int i = 0; i< n; i++){
-            digits[i] = sc.nextInt();
-        }
-        Solution s = new Solution();
-        int[] ans = s.plusOne(digits);
-        System.out.println("New array : ");
-        for(int i = 0; i< ans.length; i++){
-            System.out.print(digits[i] + " ");
-        }
-        sc.close();
     }
 }
