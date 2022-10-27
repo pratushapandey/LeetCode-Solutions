af995df7c18c60b56dc07de4b9741f0c801b259c
Pratusha Pandey <pratushapandey08@gmail.com>
2022-10-27 22:55:45 +0530

    Update Intersection of Two Arrays II.cpp

--- Easy/Intersection of Two Arrays II.cpp
+++ Easy/Intersection of Two Arrays II.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(),nums1.end())
+        sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
         
         int i=0,j=0;
